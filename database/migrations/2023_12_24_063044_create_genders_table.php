e74da578a6750c09790faa891ffcb104eeb217be
Jose Rosa <jrosamanan@gmail.com>
2023-12-24 02:33:14 -0400

    Build Gender model

---
+++ database/migrations/2023_12_24_063044_create_genders_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('genders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('creator_id');
+            $table->unsignedBigInteger('updater_id')->nullable();
+            $table->unsignedBigInteger('deleter_id')->nullable();
+
+            $table->foreign('creator_id')
+                        ->on('users')
+                        ->references('id')
+                        ->restrictOnDelete();
+
+            $table->foreign('updater_id')
+                        ->on('users')
+                        ->references('id')
+                        ->restrictOnDelete();
+
+            $table->foreign('deleter_id')
+                        ->on('users')
+                        ->references('id')
+                        ->restrictOnDelete();
+        });
+
+        Schema::table( 'users', function ( Blueprint $table ) {
+            $table->foreign('gender_id')
+                        ->on('genders')
+                        ->references('id')
+                        ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table( 'genders', function ( Blueprint $table ) {
+            $table->dropForeign(['creator_id']);
+            $table->dropForeign(['updater_id']);
+            $table->dropForeign(['deleter_id']);
+        });
+
+        Schema::table( 'users', function ( Blueprint $table ) {
+            $table->dropForeign(['gender_id']);
+        });
+
+
+        Schema::dropIfExists('genders');
+    }
+};
