69a3541bd15d479be2eb72c51169cb2404521cea
Jose Rosa <jrosamanan@gmail.com>
2023-12-24 06:19:17 -0400

    Build Country model

---
+++ app/Models/Country.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Country extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'name',
+        'creator_id',
+        'updater_id',
+        'deleter_id',
+    ];
+
+    /**
+     * 
+     *  Mutators and Accessors
+     *  
+     *  
+     * */
+
+    protected function name () : Attribute 
+    {
+        return Attribute::make(
+            set: fn ( string $value ) => ucwords( strtolower($value) ),
+        );
+    } 
+
+    /**
+     * 
+     *  Relations
+     *  
+     *  
+     * */
+
+    public function creator () : BelongsTo
+    {
+        return $this->belongsTo( User::class, 'creator_id' );
+    }
+
+    public function updater () : BelongsTo
+    {
+        return $this->belongsTo( User::class, 'updater_id' );
+    }
+
+    public function deleter () : BelongsTo
+    {
+        return $this->belongsTo( User::class, 'deleter_id' );
+    }
+
+}
---
+++ database/migrations/2023_12_24_064015_create_countries_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('creator_id');
+            $table->unsignedBigInteger('updater_id')->nullable();
+            $table->unsignedBigInteger('deleter_id')->nullable();
+
+            $table->foreign('creator_id')
+                        ->on('users')
+                        ->references('id')
+                        ->restrictOnDelete();
+
+            $table->foreign('updater_id')
+                        ->on('users')
+                        ->references('id')
+                        ->restrictOnDelete();
+
+            $table->foreign('deleter_id')
+                        ->on('users')
+                        ->references('id')
+                        ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+        Schema::table( 'countries', function ( Blueprint $table ) {
+            $table->dropForeign(['creator_id']);
+            $table->dropForeign(['updater_id']);
+            $table->dropForeign(['deleter_id']);
+        });
+
+        Schema::dropIfExists('countries');
+    }
+};
