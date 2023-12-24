c4aa3beefda8fc95d79fd0d208adccafb5d5129b
Jose Rosa <jrosamanan@gmail.com>
2023-12-24 09:11:46 -0400

    Build Station model

--- app/Models/StationSetting.php
+++ app/Models/StationSetting.php
@@ -7,5 +7,63 @@ use Illuminate\Database\Eloquent\Model;
 
 class StationSetting extends Model
 {
-    use HasFactory;
+    
+    
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'station_id',
+        'name',
+        'value',
+        'creator_id',
+        'updater_id',
+        'deleter_id',
+    ];
+
+    /**
+     * 
+     *  Mutators and Accessors
+     *  
+     *  
+     * */
+
+    protected function name () : Attribute 
+    {
+        return Attribute::make(
+            set: fn ( string $value ) => ucwords( strtolower($value) ),
+        );
+    } 
+
+    /**
+     * 
+     *  Relations
+     *  
+     *  
+     * */
+
+    public function creator () : BelongsTo
+    {
+        return $this->belongsTo( User::class, 'creator_id' );
+    }
+
+    public function updater () : BelongsTo
+    {
+        return $this->belongsTo( User::class, 'updater_id' );
+    }
+
+    public function deleter () : BelongsTo
+    {
+        return $this->belongsTo( User::class, 'deleter_id' );
+    }
+
+    public function station () : belongsTo 
+    {
+        return $this->belongsTo( Station::class );
+    }
 }
