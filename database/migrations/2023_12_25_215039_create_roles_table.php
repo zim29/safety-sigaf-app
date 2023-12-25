53508d9589e1762395b584624a9fe8d858b06923
Jose Rosa <jrosamanan@gmail.com>
2023-12-25 17:53:41 -0400

    Build Role model

---
+++ database/migrations/2023_12_25_215039_create_roles_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('creator_id')->nullable();
+            $table->unsignedBigInteger('updater_id')->nullable();
+            $table->unsignedBigInteger('deleter_id')->nullable();
+
+            $table->foreign('creator_id')
+                        ->on('users')
+                        ->references('id')
+                        ->restrictOnDelete();
+
+            $table->foreign('updater_id')
+                        ->on('users')
+                        ->references('id')
+                        ->restrictOnDelete();
+
+            $table->foreign('deleter_id')
+                        ->on('users')
+                        ->references('id')
+                        ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
