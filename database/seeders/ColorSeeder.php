73e730da4580e10a7d11d05576e242a71403d9ac
Jose Rosa <jrosamanan@gmail.com>
2023-12-27 06:38:56 -0400

    Build Color model

--- database/seeders/ColorSeeder.php
+++ database/seeders/ColorSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Color;
+
 class ColorSeeder extends Seeder
 {
     /**
@@ -12,6 +14,6 @@ class ColorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Color::factory(20)->create();
     }
 }
