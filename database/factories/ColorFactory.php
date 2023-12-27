5288f029e27411a7c2ae8422a4fa965729843757
Jose Rosa <jrosamanan@gmail.com>
2023-12-27 06:39:44 -0400

    Build Color model

--- database/factories/ColorFactory.php
+++ database/factories/ColorFactory.php
@@ -17,7 +17,7 @@ class ColorFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->unique()->colorName();
+            'name' => fake()->unique()->colorName(),
         ];
     }
 }
